55a6406cc350afbdad1bddb2c78ff062687e1cfc
GabeEllis <gabeellis830@gmail.com>
2023-04-08 22:27:10 -0400

    Fixed styling issues with navbar not saving.

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -4,28 +4,25 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/sudoku">Sudoku</NavLink>
-          </li>
-          <li>
-            <NavLink to="/chess">Chess</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Log in</NavLink>
-          </li>
-          <li>
-            <NavLink to="/signup">Sign up</NavLink>
-          </li>
-        </ul>
-      </nav>
-      {/* <Outlet /> */}
-    </>
+    <nav className="navbar">
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sudoku">Sudoku</NavLink>
+        </li>
+        <li>
+          <NavLink to="/chess">Chess</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Log in</NavLink>
+        </li>
+        <li>
+          <NavLink to="/signup">Sign up</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
