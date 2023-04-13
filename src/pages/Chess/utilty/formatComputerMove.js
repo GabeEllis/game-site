a4d113638a6470f863e53a2422e9959b89662387
GabeEllis <gabeellis830@gmail.com>
2023-04-13 10:10:38 -0400

    Added more comments to the utility functions for better code clarity.

--- src/pages/Chess/utilty/formatComputerMove.js
+++ src/pages/Chess/utilty/formatComputerMove.js
@@ -1,7 +1,8 @@
 import { movePiece } from "../components/Board/Board";
 
+// Converts the AI move output into a useable form so that game logic can be applied to it.
 export function formatComputerMove(object, board) {
-  // Converts the AI move output into a useable form so that game logic can be applied to it.
+  // Strips away the extra characters from the AI move.
   let stringMove = JSON.stringify(object);
   const formatString = stringMove
     .replace("{", "")
@@ -17,8 +18,6 @@ export function formatComputerMove(object, board) {
   const startingIndex = board.findIndex((tile) => tile.id === formatString[0]);
   // Gets the index where the id of the location after the piece moves for the AI move.
   const finalIndex = board.findIndex((tile) => tile.id === formatString[1]);
-
-  // const finalBoard = JSON.parse(JSON.stringify(board));
 
   return [startingIndex, finalIndex];
 }
--- src/pages/Chess/utilty/sortCapturedPieces.js
+++ src/pages/Chess/utilty/sortCapturedPieces.js
@@ -1,11 +1,15 @@
+// Sorts the array based on the value from greatest to least.
 function sortArray(array) {
   array.sort((a, b) => {
     return b.value - a.value;
   });
 }
 
+// Finds the total number of points that have been captured.
 export function findTotalPoints(array) {
+  // Intializes the total points at 0.
   let totalPoints = 0;
+  // Loops through the list of captured pieces and converts the piece values into a number.
   array.forEach((piece) => {
     if (piece === "Q" || piece === "q") {
       totalPoints += 9;
@@ -22,9 +26,11 @@ export function findTotalPoints(array) {
   return totalPoints;
 }
 
+// Sorts the captured pieces.
 export function sortCapturedPieces(capturedPiecesArray) {
+  // Intializes the sortedArray.
   let sortedArray = [];
-
+  // Loops through the list of captured pieces and assigns a value for each captured piece so that it can be sorted by value.
   capturedPiecesArray.forEach((piece) => {
     if (piece === "Q" || piece === "q") {
       sortedArray.push({ piece: piece, value: 9 });
@@ -38,6 +44,7 @@ export function sortCapturedPieces(capturedPiecesArray) {
       sortedArray.push({ piece: piece, value: 1 });
     }
   });
+  // Calls the sort function to reorder the array based on piece value.
   sortArray(sortedArray);
 
   return sortedArray;
